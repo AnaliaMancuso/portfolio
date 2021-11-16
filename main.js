c339bbbd5dae053f47fb15ab2d3333b27b239b6a
AnaliaMancuso <81722203+AnaliaMancuso@users.noreply.github.com>
2021-11-16 10:05:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    // Typing Animation
+    var typed = new Typed(".typing", {
+      strings: ["Web Designer", "puto el q lee", "hola hola"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+    
+    var typed = new Typed(".typing-2", {
+      strings: ["Web Designer", "puto el q lee"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+    });
